688b535cf41a912514644697d1e0ef5fd594f449
RossKinsella <rosskinsella0@gmail.com>
2015-05-02 23:30:13 +0100

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.compress = true
+  config.assets.compile = true
+  config.assets.digest = true
+  config.assets.initialize_on_precompile = false
 end
